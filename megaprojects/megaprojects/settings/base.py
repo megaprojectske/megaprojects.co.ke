62016c384f4b46924e16a65c6139d3e2dbeba0ce
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-05-18 23:13:44 +0300

    Add blog, programs and articles to INSTALLED_APPS

--- megaprojects/megaprojects/settings/base.py
+++ megaprojects/megaprojects/settings/base.py
@@ -190,6 +190,9 @@ THIRD_PARTY_APPS = (
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
+    'articles',
+    'programs',
+    'blog',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
