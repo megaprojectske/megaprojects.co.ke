566f2211652066fc5f6a5625494e6a70ee02f361
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-06-07 01:27:33 +0300

    Improve image rendering template tag

--- megaprojects/core/templatetags/core_tags.py
+++ megaprojects/core/templatetags/core_tags.py
@@ -8,11 +8,25 @@ register = template.Library()
 
 @register.filter(is_safe=True)
 @stringfilter
-def render_images(content):
-    return util.image_render(content)
+def render_images(value, model):
+    from os.path import join
+    from django import template
+
+    def _replace(match):
+        templates = [join('renders', 'image')]
+        tpl = template.loader.select_template(
+            ["%s.html" % p for p in templates])
+        return tpl.render(template.Context({
+            'image': util.image_parse(match, model)
+        }))
+
+    return util.image_sub(value, _replace)
 
 
 @register.filter(is_safe=True)
 @stringfilter
-def remove_images(content):
-    return util.image_remove(content)
+def remove_images(value):
+    def _replace(match):
+        return ''
+
+    return util.image_sub(value, _replace)
--- megaprojects/core/util.py
+++ megaprojects/core/util.py
@@ -1,58 +1,17 @@
-def image_parse(match):
-    """
-    re Index reference for expression:
-
-     group(1) | group(2) | group(3)
-    ==========|==========|==========
-     '<<<'    | *UUID    | '>>>'
-              |          |
-    """
-    from articles.models import Image as ArticleImage
-    from blog.models import Image as BlogImage
-    from programs.models import Image as ProgramImage
-
+def image_parse(match, model):
     try:
-        image = ArticleImage.objects.get(uuid=match.group(2))
-    except ArticleImage.DoesNotExist:
-        try:
-            image = BlogImage.objects.get(uuid=match.group(2))
-        except BlogImage.DoesNotExist:
-            try:
-                image = ProgramImage.objects.get(uuid=match.group(2))
-            except ProgramImage.DoesNotExist:
-                image = None
+        image = model.image_set.get(uuid=match.group('uuid'))
+    except model.image_set.model.DoesNotExist:
+        image = None
 
     return image
 
 
-def image_sub(content, sub_callback):
+def image_sub(content, repl):
     import re
 
-    IMAGE_RE = re.compile(
-        r'(<<<)([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})(>>>)')
-    return IMAGE_RE.sub(sub_callback, content)
-
-
-def image_render(content):
-    from os.path import join
-    from django import template
-
-    def _replace(match):
-        image = image_parse(match)
-        templates = [join('renders', 'image')]
-        tpl = template.loader.select_template(
-            ["%s.html" % p for p in templates])
-
-        return tpl.render(template.Context({'image': image}))
-
-    return image_sub(content, _replace)
-
-
-def image_remove(content):
-    def _replace(match):
-        return ''
-
-    return image_sub(content, _replace)
+    IMAGE_RE = r'\[image (?P<uuid>[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})\]'
+    return re.sub(IMAGE_RE, repl, content)
 
 
 def unique_boolean(field, subset=[]):
