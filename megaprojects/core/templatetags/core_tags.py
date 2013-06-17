304878a84d244f7a5b5c288c2c1b480093b3c5c4
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-06-17 17:50:17 +0300

    Add social media icons, facebook, twitter, googleplus and rss

--- megaprojects/core/templatetags/core_tags.py
+++ megaprojects/core/templatetags/core_tags.py
@@ -31,3 +31,80 @@ def remove_images(value):
         return ''
 
     return util.image_sub(value, _replace)
+
+
+# See: https://github.com/lettertwo/django-socialsharing
+@register.inclusion_tag('social/addthis_js.html', takes_context=True)
+def addthis_js(context, share_url=None, pubid=None, ga_tracker=None, ga_social=True, track_clickback=True, track_addressbar=True):
+    from django.conf import settings
+    from django.template import TemplateSyntaxError
+
+    """
+    addthis
+    =======
+    The addthis templatetag will insert the necessary scripts to make use of
+    the [AddThis](http://www.addthis.com) widget.
+
+    Basic usage:
+    ------------
+
+        {% addthis_js pubid=xxxxxxxxxx %}
+
+    or with `ADDTHIS_PUBID` defined in settings.py:
+
+        {% addthis_js %}
+
+    The only required argument is `pubid`. You may optionally provide a global
+    setting for this by adding `ADDTHIS_PUBID = xxxxxx` to your settings.py.
+    Setting this value means that you can omit the `pubid` argument when
+    invoking the templatetag.
+
+    Optional settings:
+    ------------------
+    `share_url`: The url to share. Pass this argument to override the default
+    AddThis value (the URL of the page being viewed). Example:
+
+        {% addthis_js 'http://myurl.com' %}
+
+    `track_clickback`: From the AddThis docs:
+
+        Set to true to allow us to append a variable to your URLs upon sharing.
+        We'll use this to track how many people come back to your content via
+        links shared with AddThis. Highly recommended.
+
+    default value is `True`.
+    **Note:** You may optionally set
+    `ADDDTHIS_TRACK_CLICKBACK = False` to always force this value to `False`.
+
+    To enable tracking via Google Analytics:
+    ----------------------------------------
+    * add `ADDTHIS_GA_TRACKING_ENABLED = True` to your settings.py
+    * add `ADDTHIS_GA_TRACKER = UA-XXXXXX-X` to your settings.py
+
+    **Note:** If you don't set `ADDDTHIS_GA_TRACKER`, the template tag will
+    attempt to fall back to `GOOGLE_ANALYTICS_PROPERTY_ID` (which is used by
+    [django-analytical](http://github.com/jcassee/django-analytical)).
+    """
+
+    if getattr(settings, 'ADDTHIS_GA_TRACKING_ENABLED', False):
+        if ga_tracker is None:
+            ga_tracker = getattr(settings, 'ADDTHIS_GA_TRACKER', getattr(
+                settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', None))
+        if ga_tracker is None:
+            raise TemplateSyntaxError(
+                'The addthis template tag is configured to use Google Analytics, but a tracking code was not found. You must either pass it as an argument or set ADDTHIS_GA_TRACKER in settings.py')
+
+    if pubid is None:
+        pubid = getattr(settings, 'ADDTHIS_PUBID', None)
+    if pubid is None:
+        raise TemplateSyntaxError(
+            'The addthis template tag requires a pubid. You must either pass it as an argument or set ADDTHIS_PUBID in settings.py.')
+
+    return {
+        'data_ga_property': ga_tracker,
+        'data_ga_social': ga_social,
+        'data_track_addressbar': track_addressbar,
+        'data_track_clickback': track_clickback,
+        'pubid': pubid,
+        'share_url': share_url,
+    }
