f3feb7aa78267e1a950aeae05b1b0796f1f177cf
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-06-20 02:33:46 +0300

    Set configurations for production

--- megaprojects/megaprojects/settings/production.py
+++ megaprojects/megaprojects/settings/production.py
@@ -48,13 +48,30 @@ SERVER_EMAIL = EMAIL_HOST_USER
 
 
 ########## DATABASE CONFIGURATION
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_env_setting('DATABASE_NAME'),
+        'USER': get_env_setting('DATABASE_USER'),
+        'PASSWORD': get_env_setting('DATABASE_PASS'),
+        'HOST': get_env_setting('DATABASE_HOST'),
+        'PORT': get_env_setting('DATABASE_PORT'),
+    }
+}
 ########## END DATABASE CONFIGURATION
 
 
 ########## CACHE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = {}
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'LOCATION': get_env_setting('CACHE_LOCATION'),
+    }
+}
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#cache-middleware-anonymous-only
+CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
 ########## END CACHE CONFIGURATION
 
 
@@ -62,3 +79,39 @@ CACHES = {}
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = get_env_setting('SECRET_KEY')
 ########## END SECRET CONFIGURATION
+
+
+########## SECURITY CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = get_env_setting('ALLOWED_HOSTS')
+########## END SECURITY CONFIGURATION
+
+
+########## STATIC FILE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+STATIC_ROOT = get_env_setting('STATIC_ROOT')
+########## END STATIC FILE CONFIGURATION
+
+
+########## DJANGO ANALYTICAL CONFIGURATION
+# See: http://pythonhosted.org/django-analytical/features.html#identifying-visitors
+ANALYTICAL_AUTO_IDENTIFY = get_env_setting('ANALYTICAL_AUTO_IDENTIFY')
+
+# See: http://pythonhosted.org/django-analytical/services/google_analytics.html#setting-the-property-id
+GOOGLE_ANALYTICS_PROPERTY_ID = get_env_setting('GOOGLE_ANALYTICS_PROPERTY_ID')
+
+# See: http://pythonhosted.org/django-analytical/services/google_analytics.html#tracking-multiple-domains
+GOOGLE_ANALYTICS_TRACKING_STYLE = get_env_setting('GOOGLE_ANALYTICS_TRACKING_STYLE')
+########## END DJANGO ANALYTICAL CONFIGURATION
+
+
+########## ADDTHIS CONFIGURATION
+ADDTHIS_PUB_ID = get_env_setting('ADDTHIS_PUB_ID')
+
+ADDTHIS_GA_TRACKING_ENABLED = get_env_setting('ADDTHIS_GA_TRACKING_ENABLED')
+########## END ADDTHIS CONFIGURATION
+
+
+########## INTENSEDEBATE CONFIGURATION
+INTENSEDEBATE_ACCT = get_env_setting('INTENSEDEBATE_ACCT')
+########## END INTENSEDEBATE CONFIGURATION
