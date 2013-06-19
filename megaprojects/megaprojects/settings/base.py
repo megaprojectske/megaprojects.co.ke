986cf54299462159239641f2f987d7c7e59a47c8
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-06-20 02:32:55 +0300

    Add GZip and ConditionalGet middleware

--- megaprojects/megaprojects/settings/base.py
+++ megaprojects/megaprojects/settings/base.py
@@ -149,14 +149,15 @@ TEMPLATE_DIRS = (
 ########## MIDDLEWARE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#middleware-classes
 MIDDLEWARE_CLASSES = (
-    # Default Django middleware.
+    'django.middleware.gzip.GZipMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
 )
 ########## END MIDDLEWARE CONFIGURATION
 
