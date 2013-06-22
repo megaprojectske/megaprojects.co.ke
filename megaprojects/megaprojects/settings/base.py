51fdcab838687b1f456164912431d1ef488424c3
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-06-23 00:38:13 +0300

    Set sorl thumbnail configurations

--- megaprojects/megaprojects/settings/base.py
+++ megaprojects/megaprojects/settings/base.py
@@ -290,3 +290,13 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 ########## END HAYSTACK CONFIGURATION
+
+
+########## THUMBNAIL CONFIGURATION
+# See: http://sorl-thumbnail.readthedocs.org/en/latest/reference/settings.html#thumbnail-progressive
+THUMBNAIL_PROGRESSIVE = False
+# See: http://sorl-thumbnail.readthedocs.org/en/latest/reference/settings.html#thumbnail-upscale
+THUMBNAIL_UPSCALE = False
+# See: http://sorl-thumbnail.readthedocs.org/en/latest/reference/settings.html#thumbnail-format
+THUMBNAIL_FORMAT = 'PNG'
+########## THUMBNAIL CONFIGURATION
