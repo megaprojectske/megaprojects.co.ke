65ac6267558ca738c824b283d4fce963471afbd4
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-06-22 23:19:20 +0300

    Code/Template refactoring

--- megaprojects/core/templatetags/core_tags.py
+++ megaprojects/core/templatetags/core_tags.py
@@ -64,76 +64,77 @@ def remove_articles(value):
 
 
 # See: https://github.com/lettertwo/django-socialsharing
-@register.inclusion_tag('social/addthis/config_js.html', takes_context=True)
-def addthis_config(context, share_url=None, ga_social=True, track_clickback=True, track_addressbar=True, ui_use_css=True, twitter_bitly=True, twitter_via=None):
+@register.inclusion_tag('external/addthis/config_js.html')
+def addthis_config(data_ga_social=True, track_clickback=True, track_addressbar=True, ui_use_css=True, twitter_bitly=True, twitter_via=None):
     """
-    The addthis_js templatetag will insert the necessary scripts to make use of
-    the [AddThis](http://www.addthis.com) widget.
+    Inserts the necessary scripts to make use of the
+    [AddThis](http://www.addthis.com) widget.
 
-    Basic usage:
-    ------------
+    Sample usage::
 
         {% addthis_js %}
 
-    The only required argument is a global setting for this by adding
-    `ADDTHIS_PUBID = xxxxxx` to your settings.py.
+    The only required argument is a global setting ``ADDTHIS_PUBID = xxxxxx``
+    added to your settings.py.
 
-    Optional settings:
-    ------------------
-    `share_url`: The url to share. Pass this argument to override the default
-    AddThis value (the URL of the page being viewed). Example:
+    Optional arguments::
+
+        {% addthis_js [data_ga_social] [track_clickback] [track_addressbar] [ui_use_css] [twitter_bitly] [twitter_via] %}
+
+    ``share_url``: The url to share. Pass this argument to override the default
+    AddThis value (the URL of the page being viewed). Example::
 
         {% addthis_js 'http://myurl.com' %}
 
-    `track_clickback` (default = True): From the AddThis docs:
+    ``track_clickback`` (default = True): From the AddThis docs:
 
         Set to true to allow us to append a variable to your URLs upon sharing.
         We'll use this to track how many people come back to your content via
         links shared with AddThis. Highly recommended.
 
-    `track_addressbar` (default = True): TODO: Add doc
-    `twitter_via` (default = None): TODO: Add doc
-    `shorten_bitly` (default = None): TODO: Add doc
+    ``track_addressbar`` (default = True): TODO: Add doc
 
-    **Note:** You may optionally set `ADDDTHIS_TRACK_CLICKBACK = False` to
-    always force this value to `False`.
+    ``twitter_via`` (default = None): TODO: Add doc
+
+    ``shorten_bitly`` (default = None): TODO: Add doc
+
+    **Note:** You may optionally set ``ADDDTHIS_TRACK_CLICKBACK = False`` to
+    always force this value to ``False``.
 
     To enable tracking via Google Analytics:
-    ----------------------------------------
-    * add `ADDTHIS_GA_TRACKING_ENABLED = True` to your settings.py
-    * add `ADDTHIS_GA_TRACKER = UA-XXXXXX-X` to your settings.py
 
-    **Note:** If you don't set `ADDDTHIS_GA_TRACKER`, the template tag will
-    attempt to fall back to `GOOGLE_ANALYTICS_PROPERTY_ID` (which is used by
-    [django-analytical](http://github.com/jcassee/django-analytical)).
+    * add ``ADDTHIS_GA_TRACKING_ENABLED = True`` to your settings.py
+    * add ``ADDTHIS_GA_TRACKER = UA-XXXXXX-X`` to your settings.py
+
+    **Note:** If you don't set ``ADDTHIS_DATA_GA_PROPERTY``, the template tag will
+    attempt to fall back to ``GOOGLE_ANALYTICS_PROPERTY_ID``.
     """
 
     from django.conf import settings
     from django.template import TemplateSyntaxError
 
     if getattr(settings, 'ADDTHIS_GA_TRACKING_ENABLED', False):
-        ga_tracker = getattr(settings, 'ADDTHIS_GA_TRACKER', getattr(
+        data_ga_property = getattr(settings, 'ADDTHIS_DATA_GA_PROPERTY', getattr(
             settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', None))
-        if ga_tracker is None:
+        if data_ga_property is None:
             raise TemplateSyntaxError(
-                'The addthis template tag is configured to use Google Analytics, but a tracking code was not found. You must set ADDTHIS_GA_TRACKER in settings.py')
+                'The addthis template tag is configured to use Google Analytics, but a tracking code was not found. You must set ADDTHIS_DATA_GA_PROPERTY in settings.py')
     else:
-        ga_tracker = None
+        data_ga_property = None
 
     return {
-        'data_ga_property': ga_tracker,
-        'data_ga_social': ga_social,
+        'data_ga_property': data_ga_property,
+        'data_ga_social': data_ga_social,
         'data_track_addressbar': track_addressbar,
         'data_track_clickback': track_clickback,
-        'share_url': share_url,
         'twitter_bitly': twitter_bitly,
         'twitter_via': twitter_via,
         'ui_use_css': ui_use_css,
     }
 
 
-@register.inclusion_tag('social/addthis/widget_js.html', takes_context=True)
-def addthis_widget(context):
+@register.inclusion_tag('external/addthis/widget_js.html')
+def addthis_widget():
     from django.conf import settings
     from django.template import TemplateSyntaxError
 
@@ -147,8 +148,8 @@ def addthis_widget(context):
     }
 
 
-@register.inclusion_tag('social/intensedebate/config_js.html', takes_context=True)
-def intensedebate_config(context, post_id=None, post_url=None, post_id_prefix='', post_id_suffix=''):
+@register.inclusion_tag('external/intensedebate/config_js.html')
+def intensedebate_config(post_id=None, post_url=None, post_id_prefix='', post_id_suffix=''):
     from django.conf import settings
     from django.template import TemplateSyntaxError
 
@@ -168,18 +169,18 @@ def intensedebate_config(context, post_id=None, post_url=None, post_id_prefix=''
     }
 
 
-@register.inclusion_tag('social/intensedebate/comment_wrapper_js.html', takes_context=True)
-def intensedebate_cw(context):
+@register.inclusion_tag('external/intensedebate/load_wrapper_js.html')
+def intensedebate_cw():
     return
 
 
-@register.inclusion_tag('social/intensedebate/link_wrapper_js.html', takes_context=True)
-def intensedebate_lw(context):
+@register.inclusion_tag('external/intensedebate/link_wrapper_js.html')
+def intensedebate_lw():
     return
 
 
-@register.inclusion_tag('analytics/google_analytics/analytics_js.html', takes_context=True)
-def analytics_load(context):
+@register.inclusion_tag('external/google_analytics/analytics_js.html')
+def analytics_load():
     from django.conf import settings
     from django.template import TemplateSyntaxError
 
