3abb08c52670c5486b450bcebffbe98e62835ff0
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2014-06-10 15:26:01 +0300

    Lower case filenames

--- megaprojects/programs/utils.py
+++ megaprojects/programs/utils.py
@@ -1,8 +1,7 @@
 def get_image_path(instance, filename):
     import re
 
-    folder_name = '%s/%s' % ('programs', instance.program.id)
+    folder_name = '%s/%s' % ('programs', instance.article.id)
     extension = re.search(r'\.[^.]+$', filename)
-    filename = str(instance.shortuuid) + extension.group(0)
-
-    return '%s/%s' % (folder_name, filename)
+    new_filename = str(instance.shortuuid) + extension.group(0)
+    return '%s/%s' % (folder_name, new_filename.lower())
